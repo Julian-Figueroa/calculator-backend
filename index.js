7d0f2746f1ce43528e2ea25b0ad89ad1fcba77de
Julian-Figueroa <julianfigueroatorres@gmail.com>
2023-05-26 12:37:12 -0500

    Add vercel.json file and remove a lot of unwanted scripts commands in package.json file

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const cors  = require("cors");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
 app.use((_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-    );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-    );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
+});
+
+app.get('/', (_, res) => {
+  res.send('Express JS on Vercel');
 });
 
 require('./routes/health')(app);
