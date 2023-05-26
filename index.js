0cfc544038e4c95e920fe7f0951fca55c33c4901
Julian-Figueroa <julianfigueroatorres@gmail.com>
2023-05-26 11:59:25 -0500

    Add cors and body parser to the index.js file

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors  = require("cors");
 
 const app = express();
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(cors());
+
+app.use((_, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+  next();
+});
 
 require('./routes/health')(app);
 require('./routes/operation')(app);
--- routes/health.js
+++ routes/health.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   // @route GET /
   // @desc Health check of the API
   // @access Public
-  app.get('/', (_, res) => {
+  app.get('/health', (_, res) => {
     res.json({ message: 'OK' });
   });
 };
